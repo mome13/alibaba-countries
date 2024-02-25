99921f6d0179617376f60a4370328fce0689c76f
mehran13mome <mehran13mome@gmail.com>
2024-02-25 16:47:05 +0330

    Add dark mode
      -  add useTheme to change theme

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,13 +1,18 @@
 import './navbar.css';
+import { useTheme } from '@/hooks/useTheme';
 
 const Navbar = () => {
+  const [theme, toggleTheme] = useTheme();
+
   return (
     <div className='navbar'>
       <div className='container flex justify-between items-center'>
         <h1>{'Where in the world?'}</h1>
-        <button className='btn-theme'>
+        <button onClick={() => toggleTheme()} className='btn-theme'>
           <i className='btn-theme__icon fa-solid fa-moon'></i>
-          <span className='semi-bold'>{'Dark Mode'}</span>
+          <span className='semi-bold'>{`${
+            theme === 'Dark' ? 'Light' : 'Dark'
+          } Mode`}</span>
         </button>
       </div>
     </div>

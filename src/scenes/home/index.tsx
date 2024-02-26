5f8271556a729d277d2e346e2f002993169f3512
mehran13mome <mehran13mome@gmail.com>
2024-02-26 15:19:07 +0330

    Add filter section for home screen

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,18 +1,19 @@
 import Dropdown from '@/components/dropdown';
 import FilterByRegion from './features/filter-by-region';
-
+import Search from '@/components/search';
+import './home.css';
 
 const Home = () => {
   return (
-    <div>
-      Home
-      <Dropdown title='Filter by Region'>
-        <FilterByRegion />
-      </Dropdown>
-    </div>
+    <main className='container'>
+      <div className='home-filters'>
+        <Search />
+        <Dropdown title='Filter by Region'>
+          <FilterByRegion />
+        </Dropdown>
+      </div>
+    </main>
   );
 };
-
-
 
 export default Home;

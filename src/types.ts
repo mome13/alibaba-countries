d42c539bdfeb052ded0ec1d13bba500303ed7ce9
mehran13mome <mehran13mome@gmail.com>
2024-02-27 15:41:59 +0330

    Add country item tiles

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,25 @@ export enum Themes {
   Dark = 'Dark',
   Light = 'Light',
 }
+
+export interface ICountry {
+  name: string;
+  capital: string;
+  region: string;
+  population: number;
+  flag: string;
+  nativeName: string;
+  subregion: string;
+  topLevelDomain: Array<string>;
+  currencies: Array<{
+    code: string;
+    name: string;
+    symbol: string;
+  }>;
+  languages: Array<{
+    name: string;
+    nativeName: string;
+  }>;
+  borders: Array<string> | undefined;
+  alpha3Code: string;
+}
